593d1320b7bc7b0e6b64e756d3b04772843171b3
AlphaRange001 <vp181431@gmail.com>
2025-05-25 10:59:27 +0530

    Learning how to connect frontend and backend

---
+++ FrontendBackend/index.js
@@ -0,0 +1,26 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'a joke',
+            content: 'this is a koji'
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 5040;
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
